975fd7f6db38645486d9c8658b80154e455e0c81
Rifqi14 <rifqi.persie@gmail.com>
2021-08-26 16:10:50 +0700

    Test clone

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-        title: 'Whatsapp',
+        title: 'GDE Inventory',
         debugShowCheckedModeBanner: false,
         theme: ThemeData(
           accentColor: Styles.primaryColor,
